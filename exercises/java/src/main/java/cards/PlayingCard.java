bbaa824e0292d04856806df88008db949d99a9ca
Agaton Kjellberg <agaton.kjellberg@autotrader.co.uk>
2025-03-11 13:49:57 +0000

    Yes, first one

---
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -0,0 +1,13 @@
+package cards;
+
+public class PlayingCard {
+
+
+        public int suit;
+        public int faceValue;
+
+        public PlayingCard(int suit, int faceValue) {
+                this.suit = suit;
+                this.faceValue = faceValue;
+        }
+}
